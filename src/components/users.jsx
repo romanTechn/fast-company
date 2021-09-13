4865677244c0c968546c378d111be9f2c1f130ac
romanTechn <rom.okspb@gmail.com>
2021-09-13 18:18:10 +0300

    pagination

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
 import User from "./user";
 
-const Users = ({ users, ...rest }) => {
+const Users = ({ users: allUsers, ...rest }) => {
+  const count = allUsers.length;
+  const pageSize = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (pageIndex) => {
+    console.log("page: ", pageIndex);
+    setCurrentPage(pageIndex);
+  };
+
+  const users = paginate(allUsers, currentPage, pageSize);
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Имя</th>
-          <th scope="col">Качества</th>
-          <th scope="col">Профессия</th>
-          <th scope="col">Встретился, раз</th>
-          <th scope="col">Оценка</th>
-          <th scope="col">Избранное</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => (
-          <User key={user._id} {...user} {...rest} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col">Избранное</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <User key={user._id} {...user} {...rest} />
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
   );
 };
 
